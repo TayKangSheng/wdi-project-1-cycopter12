e83ee93b2d4637923a3b020e10efe7c33479ca71
andre the <andre.the12@gmail.com>
2017-02-01 14:58:27 +0800

    added css and new pictures

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,37 +3,35 @@ window.addEventListener('DOMContentLoaded', function () {
   var score = document.querySelector('.score')
   var timerId = document.querySelector('.time')
   var button = document.querySelector('.btn')
+  var error = document.querySelector('.error')
   var allWords
   var typed
-  var seconds = 60
+  var seconds = 30
   var points = 0
-  var timerCount = 0
   var error_c = 0
-  var error = document.querySelector('.error')
+  var global_c = 0
 
   var list = ['chlordiazepoxide', 'jimmy', 'symmachus', 'picker', 'treelessness',
     'pettishness', 'gangliest', 'saehrimnir', 'gnosticiser', 'circumambience', 'catching', 'precharted', 'permanganic', 'nondivulgence', 'claystone', 'crownpiece', 'alorton', 'predoubt', 'confetti', 'evanescing', 'tenner', 'overaffliction', 'realize', 'ossian',
     'sakhalin', 'eclamptic', 'goldarnedest', 'primine', 'habitacle', 'cogitate', 'transiently', 'interaxis', 'resuit', 'gastrocnemius', 'taking', 'unopted', 'appealer', 'azbine', 'phosphocreatine', 'nalchik', 'gentilesse'
   ]
 
-  function countDown () {
-    points = 0
-    setTimeout(updateTimer, 1000)
-  }
+  // function countDown () {
+  //   points = 0
+  //   setTimeout(updateTimer, 1000)
+  // }
 
   function updateTimer () {
-    // var startIn = setInterval(function () {
     button.disabled = true
     seconds--
     timerId.innerHTML = seconds
     if (seconds === 0) {
-      alert('Time is Up! Here is your Score ' + points + 'You commited ' + error_c + 'errors')
-      // score.innerHTML = '0'
+      alert('Time is Up! Here is your Score ' + points + '. You commited ' + error_c + ' errors.')
       words.innerHTML = ''
       button.disabled = false
-      timerId.innerHTML = '60'
+      timerId.innerHTML = '30'
       // clearInterval( startIn)
-      seconds = 60
+      seconds = 30
       button.disabled = false
     } else {
       setTimeout(updateTimer, 1000)
@@ -42,7 +40,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
   function newWord () {
     words.innerHTML = ''
-    var randomWordIndex = Math.floor(Math.random() * 41)
+    var randomWordIndex = Math.floor(Math.random() * list.length)
     var randomWord = list[randomWordIndex]
 
     // randomWord is the word to appear for game
@@ -55,63 +53,36 @@ window.addEventListener('DOMContentLoaded', function () {
       span.className = 'wordsCharacter'
       words.appendChild(span)
     }
-    // words.textContent = randomWord;
-
-    // for (var i = 0; i < sentenceArr.length; i++) {
-    //   var newListArr = document.createElement('newListArr')
-    //   newListArr.classList.add('newListArr')
-    //   newListArr.innerHTML = sentenceArr[i]
-    //   words.appendChild(newListArr)
-    // }
-    // allSentence = document.querySelectorAll('div')
     allWords = randomWord
   }
 
   button.addEventListener('click', function (e) {
     updateTimer()
     newWord()
-    // countDown()
     button.disabled = true
   })
 
-  var global_c = 0
-
   function keyCheck (e) {
     // typed = keydown character.toLowerCase
-    // find the dom element that we want -> span
-    // get char of the dom Element -> toCheck
-    // check toCheck === typed
-    // if true
-    //    highlight
-    //    global counter ++
-    // else
-    //    error
+      // find the dom element that we want -> span
+      // get char of the dom Element -> toCheck
+      // check toCheck === typed
+      // if true
+      //    highlight
+      //    global counter ++
+      // else
+      //    error
 
     typed = String.fromCharCode(e.which).toLowerCase()
     // word -> element with class .words -> p dom element
     var whatWeWant = words.querySelectorAll('.wordsCharacter')[global_c] // array of spans
-    if (whatWeWant.innerHTML === typed) { // if typed letter is the one from the word
+    if (whatWeWant.innerHTML === typed) { // if typed letter is the from the word
       whatWeWant.classList.add('bg')
       global_c++
     } else {
       error_c++
       error.innerHTML = error_c
     }
-    // for (var i = 0; i < whatWeWant.length; i++) {
-    // }
-  // }
-
-  // typed = String.fromCharCode(e.which).toLowerCase()
-  // if (allSentence[global_c].innerHTML === typed) { // if typed letter is the one from the word
-  //   allSentence[global_c].classList.add('bg')
-  //   global_c++
-  // }
-
-  // else if (words[div].innHTML !== typed) { // if typed letter is the one from the word
-  //   error_c++
-  //   error.innerHTML = error_c
-  // }
-
   // animation for horse movement
     var margin1 = points * 30
     var horsey = document.getElementById('horse')
@@ -125,7 +96,7 @@ window.addEventListener('DOMContentLoaded', function () {
       document.removeEventListener('keydown', keyCheck, false)
       setTimeout(function () {
         words.className = 'words' // restart the classes
-        newWord() // give another sentence
+        newWord() // give another word
         document.addEventListener('keydown', keyCheck, false)
       }, 400)
     }
